33cd024208278ecfe858ead1545ee382778be1fb
dusssi <dushy098@gmail.com>
2026-06-22 13:46:51 +0530

    Phase 2 step 5 memory integrated with orchestrator

--- backend/app/agents/orchestrator.py
+++ backend/app/agents/orchestrator.py
@@ -1,6 +1,14 @@
 from app.state.state import AgentState
 from app.planner.planner import Planner
 
+from app.memory.models import (
+    MemoryRecord
+)
+
+from app.memory.memory_manager import (
+    MemoryManager
+)
+
 
 class OrchestratorAgent:
 
@@ -8,10 +16,23 @@ class OrchestratorAgent:
 
         self.planner = Planner()
 
+        self.memory_manager = (
+            MemoryManager()
+        )
+
     def run(
         self,
         goal: str
     ) -> AgentState:
+
+        # Load Memory
+        memory = self.memory_manager.load_memory(
+            "dushyant"
+        )
+
+        if memory:
+
+            goal = memory.goal
 
         # Create State
         state = AgentState(
@@ -23,9 +44,21 @@ class OrchestratorAgent:
             state
         )
 
+        # Save Memory
+        memory_record = MemoryRecord(
+            user_id="dushyant",
+            goal=state.user_goal,
+            completed_tasks=state.completed_tasks,
+            observations=state.observations
+        )
+
+        self.memory_manager.save_memory(
+            memory_record
+        )
+
         # Generate Response
         state.final_response = (
-            f"Goal: {goal}\n\n"
+            f"Goal: {state.user_goal}\n\n"
             f"Plan Generated Successfully"
         )
 
---
+++ backend/test_memory_runtime.py
@@ -0,0 +1,13 @@
+from app.agents.orchestrator import (
+    OrchestratorAgent
+)
+
+agent = OrchestratorAgent()
+
+result = agent.run(
+    "Get AI Internship"
+)
+
+print(result)
+
+print("\nMemory Saved Successfully")
